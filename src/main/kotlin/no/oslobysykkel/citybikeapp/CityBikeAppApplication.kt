c716f55c410eba2e3d211de49b4faf76d3ba7270
Leo Martin Westby <leo.westby@knowit.no>
2021-04-10 17:15:24 +0200

    Hack: Accept all origins (CORS)
    Can't get npm proxy or @Crossorigin annotation to work. Must fix later!

--- src/main/kotlin/no/oslobysykkel/citybikeapp/CityBikeAppApplication.kt
+++ src/main/kotlin/no/oslobysykkel/citybikeapp/CityBikeAppApplication.kt
@@ -2,10 +2,27 @@ package no.oslobysykkel.citybikeapp
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 
 @SpringBootApplication
 class CityBikeAppApplication
 
 fun main(args: Array<String>) {
     runApplication<CityBikeAppApplication>(*args)
+}
+
+@Configuration
+class MyConfiguration {
+    @Bean
+    fun corsConfigurer(): WebMvcConfigurer {
+        return object : WebMvcConfigurerAdapter() {
+            override fun addCorsMappings(registry: CorsRegistry) {
+                registry.addMapping("/**")
+            }
+        }
+    }
 }
