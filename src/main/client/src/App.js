4e11f12da8bb65584d7f6ffa8a5f8b2630bbe41a
Leo Martin Westby <leo.westby@knowit.no>
2021-04-10 17:18:33 +0200

    Show basic bike station status in frontend

--- src/main/client/src/App.js
+++ src/main/client/src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component {
+  state = {
+    isLoading: true,
+    isError: false,
+    stations: [],
+    stationsUpdatedAt: new Date(0)
+  };
+
+  async componentDidMount() {
+    const response = await fetch('http://localhost:8080/api/bikeStatus');
+    if (response.status == 200) {
+      const body = await response.json();
+      const updatedAt = new Date(body.lastUpdated * 1000);
+      this.setState({
+        stations: body.data.stations,
+        stationsUpdatedAt: updatedAt,
+        isLoading: false,
+        isError: false
+      });
+    } else {
+      this.setState({isLoading: false, isError: true});
+    }
+  }
+
+  render() {
+    const {isLoading, stations, stationsUpdatedAt, isError} = this.state;
+    if (isLoading) {
+      return <h2>Loading...</h2>
+    }
+
+    if (isError) {
+      return <h2>Bysykkel er dessverre for øyeblikket utilgjengelig. Besøk oss gjerne igjen litt senere!</h2>
+    }
+
+    return <div className="App">
+      <h2>Liste over stasjoner</h2>
+      <table className="table table-hover">
+        <thead>
+          <tr>
+            <th scope="col">Lokasjon</th>
+            <th scope="col">Ledige sykler</th>
+            <th scope="col">Ledige låser</th>
+          </tr>
+        </thead>
+        {stations.map(station =>
+          <tr key={station.id}>
+            <td>{station.station_id}</td>
+            <td>{station.num_bikes_available}</td>
+            <td>{station.num_docks_available}</td>
+          </tr>)
+        }
+      </table>
     </div>
-  );
+  }
 }
 
 export default App;
